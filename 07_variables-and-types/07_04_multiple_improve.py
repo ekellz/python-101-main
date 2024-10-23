cca260e33f95d55484ea068519d73e90450cc381
Erica <ekellz100@gmail.com>
2024-10-23 12:56:47 -0500

    Completed labs 07_01-07_06

--- 07_variables-and-types/07_04_multiple_improve.py
+++ 07_variables-and-types/07_04_multiple_improve.py
@@ -1,4 +1,7 @@
 # Improve the variable assignment shown below.
 # Why should you change it?
+    # I would keep them separate as I put below as they are different data types and helps for readability.
 
 color, year = "red", 1900
+color = "red"
+year = 1900
--- 07_variables-and-types/07_06_whats_my_type.py
+++ 07_variables-and-types/07_06_whats_my_type.py
@@ -1,7 +1,14 @@
-# What data type is the variable `mystery` at the end of the script?
+# What data type is the variable `mystery` at the end of the script? 
+    # Float
 # What data types does it hold at certain points earlier in the script?
+    # NoneType, str, int
 
 mystery = None
 mystery = "Sommerfeld"
 mystery = 137
 mystery = mystery + 0.0
+
+print(type(None))
+print(type("sommerfeld"))
+print(type(137))
+print(type(0.0))
